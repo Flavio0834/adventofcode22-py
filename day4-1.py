85be329e0886021efcba6a674d64cc6777d79ebb
Flavio <flaviosestu@yahoo.fr>
2022-12-05 17:17:14 +0100

    Day 4 (6 min + 50 secs)

---
+++ day4-1.py
@@ -0,0 +1,16 @@
+res = 0
+
+while True:
+    try:
+        a, b = input().split(",")
+        a1, a2 = map(int, a.split("-"))
+        b1, b2 = map(int, b.split("-"))
+        a = range(a1, a2 + 1)
+        b = range(b1, b2 + 1)
+        if (a[0] in b and a[-1] in b) or (b[0] in a and b[-1] in a):
+            res += 1
+        continue
+    except:
+        break
+
+print(res)
